9354e0ed3833ba5a6483a2cfe33a4aafca1e3320
Usain301 <73827957+Usain301@users.noreply.github.com>
2021-09-14 00:53:29 +0300

    İnteherincaDemo- 12-2

---
+++ İnheritanceDemos/src/inheritancedemos/BaseKrediManager.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inheritancedemos;
+
+/**
+ *
+ * @author JET BİLİŞİM
+ */
+public class BaseKrediManager {
+    
+    public void hesapla(){
+    
+        System.out.println("Krediniz hesaplanmıştır");
+    }
+    
+    public void sil(){
+    
+        System.out.println("krediniz silinmiştir");
+    }
+    
+}
